a92edb7a2629b26cd1153f347e33a12f8a0953e2
analog-sombra <analogsombra@gmail.com>
2025-09-19 12:13:05 +0530

    build: att4

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import cron from "node-cron";
 import crypto from "crypto";
 import csrf from "csurf";
 const csrfProtection = csrf();
+import express from "express";
 
 function encrypt(input, key) {
   const iv = key.slice(0, 16);
@@ -24,7 +25,9 @@ function decrypt(cipherText, key) {
 
 const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
+const hostname = "localhost";
+
+const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
 var MerchantId = "1000605";
@@ -36,8 +39,8 @@ var TotalDueAmount = "10";
 var OtherDetails = "NA";
 // var SuccessURL = "https://test.sbiepay.sbi/secure/sucess3.jsp";
 // var FailURL = "https://test.sbiepay.sbi/secure/fail3.jsp";
-var SuccessURL = "http://194.238.22.198:6789/payment/success";
-var FailURL = "http://194.238.22.198:6789/payment/fail";
+var SuccessURL = "https://dddnhvat.com/payment/success";
+var FailURL = "https://dddnhvat.com/payment/fail";
 var AggregatorId = "SBIEPAY";
 var MerchantCustomerID = "5";
 var Paymode = "NB";
@@ -46,49 +49,45 @@ var TransactionSource = "ONLINE";
 // var MerchantOrderNo = "4573243384";
 var MerchantOrderNo = new Date().getTime().toString();
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    if (parsedUrl.pathname === "/sbiepay") {
-      // your SBI logic here
-      try {
-        const Single_Request =
-          MerchantId +
-          "|" +
-          OperatingMode +
-          "|" +
-          MerchantCountry +
-          "|" +
-          MerchantCurrency +
-          "|" +
-          TotalDueAmount +
-          "|" +
-          OtherDetails +
-          "|" +
-          SuccessURL +
-          "|" +
-          FailURL +
-          "|" +
-          AggregatorId +
-          "|" +
-          MerchantOrderNo +
-          "|" +
-          MerchantCustomerID +
-          "|" +
-          Paymode +
-          "|" +
-          Accesmedium +
-          "|" +
-          TransactionSource;
+const sbiepay = () => (req, res) => {
+  try {
+    const Single_Request =
+      MerchantId +
+      "|" +
+      OperatingMode +
+      "|" +
+      MerchantCountry +
+      "|" +
+      MerchantCurrency +
+      "|" +
+      TotalDueAmount +
+      "|" +
+      OtherDetails +
+      "|" +
+      SuccessURL +
+      "|" +
+      FailURL +
+      "|" +
+      AggregatorId +
+      "|" +
+      MerchantOrderNo +
+      "|" +
+      MerchantCustomerID +
+      "|" +
+      Paymode +
+      "|" +
+      Accesmedium +
+      "|" +
+      TransactionSource;
 
-        console.log("Request String:\n" + Single_Request);
-        const value = encrypt(Single_Request, Array_key);
+    console.log("Request String:\n" + Single_Request);
+    const value = encrypt(Single_Request, Array_key);
 
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(
-          `<html><body>
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(
+      `<html><body>
              <form name="form1" method="post" action="https://test.sbiepay.sbi/secure/AggregatorHostedListener">
-             <p>${MerchantOrderNo}</p>  
+             <p>${MerchantOrderNo}</p>
              <table>
                  <tr>
                    <th>Encrypted Transaction</th>
@@ -105,22 +104,74 @@ app.prepare().then(() => {
                </table>
              </form>
            </body></html>`
-        );
-      } catch (error) {
-        console.error("SBI Pay error:", error);
-        res.writeHead(500).end("Server error");
-      }
-    } else if (parsedUrl.pathname === "/payment/success") {
-      const transactionData = req.body;
-      res.render("success_page", { data: transactionData });
-    } else if (parsedUrl.pathname === "/payment/fail") {
-      const transactionData = req.body;
-      res.render("fail_page", { data: transactionData });
-    } else {
-      handle(req, res, parsedUrl);
-    }
-    // handle(req, res, parsedUrl);
-  }).listen(port);
+    );
+  } catch (error) {
+    console.error("SBI Pay error:", error);
+    res.writeHead(500).end("Server error");
+  }
+};
+
+const sbiesuccess = () => (req, res) => {
+  res.write("Transactions Fetched Successfully");
+  res.write("<br>");
+  res.write("<br>Encrypted data =  " + req.body.encData);
+  res.write("<br>Encrypted data  body =  " + req.body);
+  // const encData = AESobj.decrypt(req.body.encData, key);
+  // res.write("<br>Decrypted data =  " + encData);
+  res.end();
+};
+const sbiefail = () => (req, res) => {
+  res.write("Transactions Failed");
+  res.write("<br>");
+  res.write("<br>Encrypted data =  " + req.body.encData);
+  res.write("<br>Encrypted data body =  " + req.body);
+  // const encData = AESobj.decrypt(req.body.encData, key);
+  // res.write("<br>Decrypted data =  " + encData);
+  res.end();
+};
+app.prepare().then(() => {
+  const server = express();
+
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: true }));
+
+  server.get("/sbiepay", (req, res) => {
+    return sbiepay()(req, res);
+  });
+
+  server.post("/payment/success", (req, res) => {
+    return sbiesuccess()(req, res);
+  });
+
+  server.post("/payment/fail", (req, res) => {
+    return sbiefail()(req, res);
+  });
+
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`------------> Ready on http://localhost:${port}`);
+  });
+
+  // createServer((req, res) => {
+  //   const parsedUrl = parse(req.url, true);
+  //   if (parsedUrl.pathname === "/sbiepay") {
+  //     // your SBI logic here
+
+  //   } else if (parsedUrl.pathname === "/payment/success") {
+  //     const transactionData = req.body;
+  //     res.render("success_page", { data: transactionData });
+  //   } else if (parsedUrl.pathname === "/payment/fail") {
+  //     const transactionData = req.body;
+  //     res.render("fail_page", { data: transactionData });
+  //   } else {
+  //     handle(req, res, parsedUrl);
+  //   }
+  //   // handle(req, res, parsedUrl);
+  // }).listen(port);
 
   console.log(
     `> Server listening at http://localhost:${port} as ${
