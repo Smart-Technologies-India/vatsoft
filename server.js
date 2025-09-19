42d45523365c36f4911d1edb4a40ead72d95cce8
analog-sombra <analogsombra@gmail.com>
2025-09-19 12:16:28 +0530

    build: att5

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ function decrypt(cipherText, key) {
   return decryptedData;
 }
 
-const port = parseInt(process.env.PORT || "3000", 10);
+const port = parseInt(process.env.PORT || "3001", 10);
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
 
