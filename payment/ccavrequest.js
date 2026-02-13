2560724aa218b4aeeb289f1d69d4ac68590a414e
analog-sombra <analogsombra@gmail.com>
2026-02-13 11:40:02 +0530

    build: url update

--- payment/ccavrequest.js
+++ payment/ccavrequest.js
@@ -20,7 +20,7 @@ export const postReq = (request, response) => {
     body += data;
     encRequest = encrypt(body, keyBase64, ivBase64);
     formbody =
-      '<form id="nonseamless" method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' +
+      '<form id="nonseamless" method="post" name="redirect" action="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' +
       encRequest +
       '"><input type="hidden" name="access_code" id="access_code" value="' +
       process.env.ACCESS_CODE +
