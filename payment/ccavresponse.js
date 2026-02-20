0b0ccfedaa905a317a6ccc61b658b26a3491ba1f
analog-sombra <analogsombra@gmail.com>
2026-02-20 16:50:09 +0530

    build: qs problem

--- payment/ccavresponse.js
+++ payment/ccavresponse.js
@@ -1,6 +1,7 @@
 import crypto from "crypto";
 import { createRequire } from "module";
 import { decrypt } from "./ccavutil.js";
+import qs from "querystring";
 
 const require = createRequire(import.meta.url);
 const prisma = require("../prisma/database.js");
